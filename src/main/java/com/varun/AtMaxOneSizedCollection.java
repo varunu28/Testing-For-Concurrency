9ec5952732c7c97ea928a10509268c0b0d737ebf
varunu28 <varun.u28@gmail.com>
2022-10-31 14:35:03 -0700

    Added unsafe collection implementation

---
+++ src/main/java/com/varun/AtMaxOneSizedCollection.java
@@ -0,0 +1,25 @@
+package com.varun;
+
+import java.util.ArrayList;
+
+/**
+ * AtMaxOneSizedCollection is an abstraction over an ArrayList where our goal is to have a collection that can have at
+ * most one entity in the data structure. So the valid states of the data structure are either empty or a collection
+ * of size one.
+ */
+public class AtMaxOneSizedCollection<T> extends ArrayList<T> {
+
+  /**
+   * Method to add an element to collection if collection is empty.
+   *
+   * @param element Element that is intended to be added to the collection
+   * @return boolean representing if the element was inserted to the collection successfully or not.
+   */
+  public boolean addIfCollectionIsEmpty(T element) {
+    boolean isEmpty = super.isEmpty();
+    if (isEmpty) {
+      super.add(element);
+    }
+    return !isEmpty;
+  }
+}
